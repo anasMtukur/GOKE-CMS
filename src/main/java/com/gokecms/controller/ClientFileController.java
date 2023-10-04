947c0a7a12236ef80794b29d341dd93d633ffd3e
anasMtukur <anasmtukur91@gmail.com>
2023-10-04 15:33:57 +0100

    Add workflow for deployment

--- src/main/java/com/gokecms/controller/ClientFileController.java
+++ src/main/java/com/gokecms/controller/ClientFileController.java
@@ -31,7 +31,7 @@ public class ClientFileController  extends BaseController {
     private static final int MAX_FILE_SIZE     = 1024 * 1024 * 1000; // 400MB
     private static final int MAX_REQUEST_SIZE  = 1024 * 1024 * 1200; // 500MB
     //private static final String UPLOAD_PATH    = "/home/ebahn/goke";
-    private static final String UPLOAD_PATH    = "/home/anas/CMS";
+    private static final String UPLOAD_PATH    = "/home/anas/CMS/uploads";
 
     
 	private ClientFileRepository repository;
